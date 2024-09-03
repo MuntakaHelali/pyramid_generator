aa239063d0c6970a1031dd4cb3a0c81a6305efe4
MuntakaH <57158277+MuntakaHelali@users.noreply.github.com>
2024-09-02 23:52:22 -0400

    Updated Script.js
    Started declaring variables and functions for Pyramid Generation based on number of Rows.

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ console.log("   ###   ");
 console.log("  #####  ");
 console.log(" ####### ");
 console.log("#########");
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount){
+    return " " + character.repeat(rowNumber)+ " ";
+}
+
+
+for (let i =0; i < count; i++) {
+  rows.push(padRow(i+1, count));
+}
+
+let result = "";
+
+for(const row of rows){
+    result += "\n"+row;
+}
+
+console.log(result);
