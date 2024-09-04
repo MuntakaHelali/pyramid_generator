a232d3f6c45e2f1324392be5b2fa62bb421acf5c
MuntakaH <57158277+MuntakaHelali@users.noreply.github.com>
2024-09-03 23:35:00 -0400

    Updated Script.js
    Solved issue with spacing and number of characters displayed for each row of pyramid. Various solutions implemented based on different types of loops and conditions.

--- script.js
+++ script.js
@@ -9,13 +9,21 @@ const count = 8;
 const rows = [];
 
 function padRow(rowNumber, rowCount){
-    return " " + character.repeat(rowNumber)+ " ";
+    return " ".repeat(rowCount-rowNumber) + character.repeat(2*rowNumber-1)+ " ".repeat(rowCount-rowNumber);
 }
 
+// TODO: use a different type of loop
+/*for (let i =1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
 
-for (let i =0; i < count; i++) {
-  rows.push(padRow(i+1, count));
-}
+/*while(rows.length < count){
+  rows.push(padRow(rows.length+1, count));
+}*/
+
+/*for(let i = count; i > 0; i--){
+  rows.push(padRow(i, count));
+}*/
 
 let result = "";
 
